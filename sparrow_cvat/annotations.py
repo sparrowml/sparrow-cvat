efad15394acdea67eeb671c436e123b8a1a96749
jbencook <ben@sparrow.dev>
2023-02-10 15:37:57 +0000

    convert augmentedboxtracking chunk to cvat

--- sparrow_cvat/annotations.py
+++ sparrow_cvat/annotations.py
@@ -7,7 +7,8 @@ from typing import Union
 
 import datumaro as dm
 import numpy as np
-from sparrow_datums import FrameAugmentedBoxes
+from sparrow_datums import AugmentedBoxTracking, FrameAugmentedBoxes
+from xmltodict import unparse
 
 
 def boxes_to_cvat(
@@ -36,3 +37,43 @@ def boxes_to_cvat(
         dataset.export(tmp_dir, "cvat")
         with open(tmp_dir / "default.xml", "r") as f1, open(output_path, "w") as f2:
             f2.write(f1.read())
+
+
+def chunk_to_cvat(
+    chunk_path: Union[str, Path], output_path: Union[str, Path], labels: list[str]
+) -> None:
+    """Convert a single AugmentedBoxTracking chunk to CVAT XML."""
+    chunk_path = Path(chunk_path)
+    output_path = Path(output_path)
+    chunk = AugmentedBoxTracking.from_file(chunk_path)
+    chunk = chunk.to_tlbr().to_absolute()
+    annotations = {"version": "1.1"}
+    tracks = []
+    for j in range(chunk.shape[1]):
+        boxes = chunk.array[:, j]
+        indices = np.argwhere(np.isfinite(boxes[:, 0])).ravel()
+        class_idx = boxes[indices[0], -1]
+        label = labels[int(class_idx)]
+        track = {"@id": str(j), "@label": label}
+        box_records = []
+        for i in indices:
+            x1, y1, x2, y2 = boxes[i, :4]
+            box_record = {
+                "@frame": str(i),
+                "@xtl": str(x1),
+                "@ytl": str(y1),
+                "@xbr": str(x2),
+                "@ybr": str(y2),
+                "@outside": "0",
+                "@occluded": "0",
+                "@keyframe": "1",
+                "@z_order": "0",
+            }
+            box_records.append(box_record)
+        box_records[-1]["@outside"] = "1"
+        track["box"] = box_records
+        tracks.append(track)
+    annotations["track"] = tracks
+    data = {"annotations": annotations}
+    with open(output_path, "w") as f:
+        f.write(unparse(data))
