147add1cf364c07735f6501f26c5969588bb8f2c
jbencook <ben@sparrow.dev>
2023-08-28 20:15:27 +0000

    remove bad imports

--- sparrow_cvat/__main__.py
+++ sparrow_cvat/__main__.py
@@ -1,8 +1,7 @@
 """Expose CLI."""
 import fire
 
-from .jobs import complete_task, download_filename_urls, list_jobs
-from .tasks import (  # upload_annotations,; upload_video,; upload_videos,
+from .tasks import (
     create_task,
     delete_task,
     download_annotations,
@@ -15,17 +14,11 @@ from .tasks import (  # upload_annotations,; upload_video,; upload_videos,
 def main() -> None:
     """Call CLI commands."""
     commands = {
-        "complete-task": complete_task,
         "delete-task": delete_task,
         "download-annotations": download_annotations,
-        "download-filename-urls": download_filename_urls,
         "download-images": download_images,
         "create-task": create_task,
-        "list-jobs": list_jobs,
         "list-tasks": list_tasks,
-        # "upload-annotations": upload_annotations,
         "upload-images": upload_images,
-        # "upload-video": upload_video,
-        # "upload-videos": upload_videos,
     }
     fire.Fire(commands)
